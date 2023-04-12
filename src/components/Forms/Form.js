7e2b9fa0c7cde6ce311d55dec3176b23f69a8a3e
Alim1899 <alimakhchanogli@gmail.com>
2023-04-12 14:24:53 +0400

    Updated button enable/disable

--- src/components/Forms/Form.js
+++ src/components/Forms/Form.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import classes from "./Form.module.css";
+
 const Form = (props) => {
   const [day, setDay] = useState("");
   const [month, setMonth] = useState("");
@@ -7,97 +8,94 @@ const Form = (props) => {
   const [button, setButton] = useState(true);
 
   const validateFields = (e) => {
-    
-      if (e.target.checkValidity()) {
-        e.target.style.outline = "1px solid green";
-        if(!e.target.value)e.target.style.outline = "1px solid red";
-        if (e.target.id === "day") {
-          setDay(e.target.value);
-        }
-        if (e.target.id === "month") {
-          setMonth(e.target.value);
-        }
-        if (e.target.id === "year") {
-          setYear(e.target.value);
-        }
-      } else {
-        e.target.style.outline = "1px solid red";
-        setTimeout(() => {
-          e.target.style.outline = "1px solid green";
-        }, 1000);
+    if (e.target.checkValidity()) {
+      e.target.style.outline = "1px solid green";
+      if (!e.target.value) e.target.style.outline = "1px solid red";
+      if (e.target.id === "day") {
+        setDay(e.target.value);
       }
-      
+      if (e.target.id === "month") {
+        setMonth(e.target.value);
+      }
+      if (e.target.id === "year") {
+        setYear(e.target.value);
+      }
+    } else {
+      e.target.style.outline = "1px solid red";
+      setTimeout(() => {
+        e.target.style.outline = "1px solid green";
+      }, 1000);
+    }
   };
 
-  const calculate = (e, dd, mm, yyyy) => {
-    const enteredDate = new Date(year, month - 1, day);
-    const now = new Date();
-    yyyy = now.getFullYear() - enteredDate.getFullYear();
-    mm = Math.abs(now.getMonth() - enteredDate.getMonth());
-    dd = Math.abs(now.getDate() - enteredDate.getDate());
-
-    if (month && day && year) {
+  const btnEnabler = (e) => {
+    if (
+      document.getElementById("day").value > 0 &&
+      document.getElementById("month").value > 0 &&
+      document.getElementById("year").value > 0
+    ) {
+      console.log('tru');
       setButton(false);
-    } else {
+    }else{
       setButton(true);
     }
   };
 
   const reset = (event) => {
     event.preventDefault();
-    props.onCalculate(day, month, year);
-    console.log(day, month, year);
     setYear("");
     setDay("");
     setMonth("");
-    setButton(true);
+    setButton(true)
   };
   return (
-    <form onSubmit={calculate}>
-      <div className={classes.form}>
-        <label htmlFor="day">
-          <h4>Day</h4>
-          <input
-            type="number"
-            max="31"
-            min="1"
-            id="day"
-            placeholder="dd"
-            maxLength="2"
-            value={day}
-            onChange={validateFields}
-          />
-        </label>
-        <label htmlFor="month">
-          <h4>Month</h4>
-          <input
-            type="number"
-            min="1"
-            max="12"
-            id="month"
-            placeholder="mm"
-            maxLength="2"
-            value={month}
-            onChange={validateFields}
-          />
-        </label>
-        <label htmlFor="year">
-          <h4>Year</h4>
-          <input
-            type="number"
-            min="1"
-            max="2023"
-            id="year"
-            value={year}
-            maxLength="4"
-            placeholder="yyyy"
-            onChange={validateFields}
-          />
-        </label>
-        <button onClick={reset} id="submit" type="submit" disabled={button}>
-          Calculate
-        </button>
-      </div>
+    <form
+      onChange={btnEnabler}
+      className={classes.form}
+      onSubmit={props.onCalculate}
+    >
+      <label htmlFor="day">
+        <h4>Day</h4>
+        <input
+          type="number"
+          max="31"
+          min="1"
+          id="day"
+          placeholder="dd"
+          maxLength="2"
+          value={day}
+          onChange={validateFields}
+        />
+      </label>
+      <label htmlFor="month">
+        <h4>Month</h4>
+        <input
+          type="number"
+          min="1"
+          max="12"
+          id="month"
+          placeholder="mm"
+          maxLength="2"
+          value={month}
+          onChange={validateFields}
+        />
+      </label>
+      <label htmlFor="year">
+        <h4>Year</h4>
+        <input
+          type="number"
+          min="1"
+          max="2023"
+          id="year"
+          value={year}
+          maxLength="4"
+          placeholder="yyyy"
+          onChange={validateFields}
+        />
+      </label>
+      <button onClick={reset} id="submit" type="submit" disabled={button}>
+        Calculate
+      </button>
     </form>
   );
 };
